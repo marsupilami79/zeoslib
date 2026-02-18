d2fa594c35be4121d7bcb4c20772af8d0dd1d8c6
marsupilami79 <marsupilami79@localhost>
2026-02-18 19:34:05 +0000

    Fixes for #647

--- src/dbc/ZDbcPostgreSqlStatement.pas
+++ src/dbc/ZDbcPostgreSqlStatement.pas
@@ -273,6 +273,8 @@ type
     procedure SetTimestamp(Index: Integer; {$IFDEF FPC_HAS_CONSTREF}constref{$ELSE}const{$ENDIF} Value: TZTimeStamp); reintroduce; overload;
 
     procedure SetBytes(Index: Integer; Value: PByte; Len: NativeUInt); reintroduce; overload;
+    procedure SetGuid(Index: Integer; {$IFDEF FPC_HAS_CONSTREF}constref{$ELSE}const{$ENDIF} Value: TGUID);
+
 
     procedure RegisterParameter(ParameterIndex: Integer; SQLType: TZSQLType;
       ParamType: TZProcedureColumnType; const Name: String = '';
@@ -2519,6 +2521,28 @@ begin
   end else if (FPQParamOIDs[InParamIdx] = OIDOID) then
     BindAsLob
   else raise CreateConversionError(Index, stBytes, SQLType);
+end;
+
+procedure TZPostgreSQLPreparedStatementV3.SetGUID(Index: Integer; {$IFDEF FPC_HAS_CONSTREF}constref{$ELSE}const{$ENDIF} Value: TGUID);
+{$IFNDEF ENDIAN_BIG}
+var
+  P: PByte;
+  D1: Cardinal;
+  D2, D3: Word;
+{$ENDIF ENDIAN_BIG}
+begin
+  {$IFNDEF ENDIAN_BIG} {$Q-} {$R-}
+    P := PByte(@Value);
+    D1 := PG2Cardinal(P);
+    PCardinal(P)^ := D1;
+    D2 := PWord(P + 4)^;
+    PWord(P + 4)^ := (D2 and $00FF shl 8) or (D2 and $FF00 shr 8);
+    D3 := PWord(P + 6)^;
+    PWord(P + 6)^ := (D3 and $00FF shl 8) or (D3 and $FF00 shr 8);
+    {$IFDEF RangeCheckEnabled} {$R+} {$ENDIF}
+    {$IFDEF OverFlowCheckEnabled} {$Q+} {$ENDIF}
+  {$ENDIF ENDIAN_BIG}
+  inherited;
 end;
 
 {**
